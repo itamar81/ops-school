74e27e89b7c65e780e3b67c05c6af1388f17bcbc
Itamar Hadad <itamarh@mprest.com>
2022-01-11 21:45:49 +0200

    fix1

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 #add fix to exercise1 here
+apt update 
+apt install resolveconf
+mkdir -p /etc/resolvconf/resolv.conf.d/
+touch /etc/resolvconf/resolv.conf.d/head
+cat > /etc/resolvconf/resolv.conf.d/head << EOF
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOF
+systemctl restart resolvconf
+
